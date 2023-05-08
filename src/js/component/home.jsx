71acc949ff935d7fe60839ae8873f63dd99ed52d
Tara <tmarieshelnutt@gmail.com>
2023-05-08 04:25:26 +0000

    redo on the project, not too bad style wise, still needs functionality.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 
+
+
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>My Todo Today List</h1>
+			<ul>
+				<li>
+					<input type="text" placeholder="What you need todo?!"></input>
+				</li>
+				<li>
+					Walk dog<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span></button>
+				</li>
+				<li>
+					Finish homework<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span></button>
+				</li>
+				<li>
+					Do laundry<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span></button>
+				</li>
+				<li>
+					Fold Laundry<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span></button>
+				</li>
+				<li>
+					Dishes<button type="button" class="close" data-dismiss="modal" aria-label="Close" float->
+          <span aria-hidden="true">&times;</span></button>
+				</li>
+			</ul>
+			<li>22 things</li>
 		</div>
 	);
 };
