03f79058d7e03214fbbc99ddb77e8054f2e9f70a
Tara <tmarieshelnutt@gmail.com>
2023-05-17 19:48:26 +0000

    more work, got the list to work, need to get the modal to cooperate.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useCallback } from "react";
+import { useState } from "react";
 
 
 //include images into your bundle
@@ -7,35 +7,33 @@ import React from "react";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+
+
 	return (
 		<div className="container">
-			<h1>My Todo Today List</h1>
+			<h1>My Todos Today</h1>
 			<ul>
 				<li>
-					<input type="text" placeholder="What you need todo?!"></input>
+					<input type="text" onChange ={(e) => setInputValue(e.target.value)}
+					value={inputValue}
+					onKeyDownCapture={(e) => {
+						if (e.key == "Enter") {
+							setTodos(todos.concat(inputValue));
+						}
+					}}
+					placeholder="What you need todo?!"></input>
 				</li>
+				{todos.map((item, index) => (
 				<li>
-					Walk dog<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					{item}<button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span></button>
 				</li>
-				<li>
-					Finish homework<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span></button>
-				</li>
-				<li>
-					Do laundry<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span></button>
-				</li>
-				<li>
-					Fold Laundry<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span></button>
-				</li>
-				<li>
-					Dishes<button type="button" class="close" data-dismiss="modal" aria-label="Close" float->
-          <span aria-hidden="true">&times;</span></button>
-				</li>
+	))}
+				
 			</ul>
-			<li>22 things</li>
+			<li>22 Problems and this list aint some...</li>
 		</div>
 	);
 };
